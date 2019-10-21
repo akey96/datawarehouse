418a32e8bacd9247c7da1d776d9f5e966c97a5a3
lurianne <luriannemarianne@gmail.com>
2019-10-20 23:31:12 -0400

    subida de archivos

---
+++ datos.sql
@@ -0,0 +1,291 @@
+--Datos
+-- Pais (script)
+
+insert into Pais(nombre) values('Afghanistan');
+insert into Pais(nombre) values('Albania');
+insert into Pais(nombre) values('Algeria');
+insert into Pais(nombre) values('Andorra');
+insert into Pais(nombre) values('Angola');
+insert into Pais(nombre) values('Antigua & Deps');
+insert into Pais(nombre) values('Argentina');
+insert into Pais(nombre) values('Armenia');
+insert into Pais(nombre) values('Australia');
+insert into Pais(nombre) values('Austria');
+insert into Pais(nombre) values('Azerbaijan');
+insert into Pais(nombre) values('Bahamas');
+insert into Pais(nombre) values('Bahrain');
+insert into Pais(nombre) values('Bangladesh');
+insert into Pais(nombre) values('Barbados');
+insert into Pais(nombre) values('Belarus');
+insert into Pais(nombre) values('Belgium');
+insert into Pais(nombre) values('Belize');
+insert into Pais(nombre) values('Benin');
+insert into Pais(nombre) values('Bhutan');
+insert into Pais(nombre) values('Bolivia');
+insert into Pais(nombre) values('Bosnia Herzegovina');
+insert into Pais(nombre) values('Botswana');
+insert into Pais(nombre) values('Brazil');
+insert into Pais(nombre) values('Brunei');
+insert into Pais(nombre) values('Bulgaria');
+insert into Pais(nombre) values('Burkina');
+insert into Pais(nombre) values('Burundi');
+insert into Pais(nombre) values('Cambodia');
+insert into Pais(nombre) values('Cameroon');
+insert into Pais(nombre) values('Canada');
+insert into Pais(nombre) values('Cape Verde');
+insert into Pais(nombre) values('Central African Rep');
+insert into Pais(nombre) values('Chad');
+insert into Pais(nombre) values('Chile');
+insert into Pais(nombre) values('China');
+insert into Pais(nombre) values('Colombia');
+insert into Pais(nombre) values('Comoros');
+insert into Pais(nombre) values('Congo');
+insert into Pais(nombre) values('Congo {Democratic Rep}');
+insert into Pais(nombre) values('Costa Rica');
+insert into Pais(nombre) values('Croatia');
+insert into Pais(nombre) values('Cuba');
+insert into Pais(nombre) values('Cyprus');
+insert into Pais(nombre) values('Czech Republic');
+insert into Pais(nombre) values('Denmark');
+insert into Pais(nombre) values('Djibouti');
+insert into Pais(nombre) values('Dominica');
+insert into Pais(nombre) values('Dominican Republic');
+insert into Pais(nombre) values('East Timor');
+insert into Pais(nombre) values('Ecuador');
+insert into Pais(nombre) values('Egypt');
+insert into Pais(nombre) values('El Salvador');
+insert into Pais(nombre) values('Equatorial Guinea');
+insert into Pais(nombre) values('Eritrea');
+insert into Pais(nombre) values('Estonia');
+insert into Pais(nombre) values('Ethiopia');
+insert into Pais(nombre) values('Fiji');
+insert into Pais(nombre) values('Finland');
+insert into Pais(nombre) values('France');
+insert into Pais(nombre) values('Gabon');
+insert into Pais(nombre) values('Gambia');
+insert into Pais(nombre) values('Georgia');
+insert into Pais(nombre) values('Germany');
+insert into Pais(nombre) values('Ghana');
+insert into Pais(nombre) values('Greece');
+insert into Pais(nombre) values('Grenada');
+insert into Pais(nombre) values('Guatemala');
+insert into Pais(nombre) values('Guinea');
+insert into Pais(nombre) values('Guinea-Bissau');
+insert into Pais(nombre) values('Guyana');
+insert into Pais(nombre) values('Haiti');
+insert into Pais(nombre) values('Honduras');
+insert into Pais(nombre) values('Hungary');
+insert into Pais(nombre) values('Iceland');
+insert into Pais(nombre) values('India');
+insert into Pais(nombre) values('Indonesia');
+insert into Pais(nombre) values('Iran');
+insert into Pais(nombre) values('Iraq');
+insert into Pais(nombre) values('Ireland {Republic}');
+insert into Pais(nombre) values('Israel');
+insert into Pais(nombre) values('Italy');
+insert into Pais(nombre) values('Ivory Coast');
+insert into Pais(nombre) values('Jamaica');
+insert into Pais(nombre) values('Japan');
+insert into Pais(nombre) values('Jordan');
+insert into Pais(nombre) values('Kazakhstan');
+insert into Pais(nombre) values('Kenya');
+insert into Pais(nombre) values('Kiribati');
+insert into Pais(nombre) values('Korea North');
+insert into Pais(nombre) values('Korea South');
+insert into Pais(nombre) values('Kosovo');
+insert into Pais(nombre) values('Kuwait');
+insert into Pais(nombre) values('Kyrgyzstan');
+insert into Pais(nombre) values('Laos');
+insert into Pais(nombre) values('Latvia');
+insert into Pais(nombre) values('Lebanon');
+insert into Pais(nombre) values('Lesotho');
+insert into Pais(nombre) values('Liberia');
+insert into Pais(nombre) values('Libya');
+insert into Pais(nombre) values('Liechtenstein');
+insert into Pais(nombre) values('Lithuania');
+insert into Pais(nombre) values('Luxembourg');
+insert into Pais(nombre) values('Macedonia');
+insert into Pais(nombre) values('Madagascar');
+insert into Pais(nombre) values('Malawi');
+insert into Pais(nombre) values('Malaysia');
+insert into Pais(nombre) values('Maldives');
+insert into Pais(nombre) values('Mali');
+insert into Pais(nombre) values('Malta');
+insert into Pais(nombre) values('Marshall Islands');
+insert into Pais(nombre) values('Mauritania');
+insert into Pais(nombre) values('Mauritius');
+insert into Pais(nombre) values('Mexico');
+insert into Pais(nombre) values('Micronesia');
+insert into Pais(nombre) values('Moldova');
+insert into Pais(nombre) values('Monaco');
+insert into Pais(nombre) values('Mongolia');
+insert into Pais(nombre) values('Montenegro');
+insert into Pais(nombre) values('Morocco');
+insert into Pais(nombre) values('Mozambique');
+insert into Pais(nombre) values('Myanmar, {Burma}');
+insert into Pais(nombre) values('Namibia');
+insert into Pais(nombre) values('Nauru');
+insert into Pais(nombre) values('Nepal');
+insert into Pais(nombre) values('Netherlands');
+insert into Pais(nombre) values('New Zealand');
+insert into Pais(nombre) values('Nicaragua');
+insert into Pais(nombre) values('Niger');
+insert into Pais(nombre) values('Nigeria');
+insert into Pais(nombre) values('Norway');
+insert into Pais(nombre) values('Oman');
+insert into Pais(nombre) values('Pakistan');
+insert into Pais(nombre) values('Palau');
+insert into Pais(nombre) values('Panama');
+insert into Pais(nombre) values('Papua New Guinea');
+insert into Pais(nombre) values('Paraguay');
+insert into Pais(nombre) values('Peru');
+insert into Pais(nombre) values('Philippines');
+insert into Pais(nombre) values('Poland');
+insert into Pais(nombre) values('Portugal');
+insert into Pais(nombre) values('Qatar');
+insert into Pais(nombre) values('Romania');
+insert into Pais(nombre) values('Russian Federation');
+insert into Pais(nombre) values('Rwanda');
+insert into Pais(nombre) values('St Kitts & Nevis');
+insert into Pais(nombre) values('St Lucia');
+insert into Pais(nombre) values('Saint Vincent & the Grenadines');
+insert into Pais(nombre) values('Samoa');
+insert into Pais(nombre) values('San Marino');
+insert into Pais(nombre) values('Sao Tome & Principe');
+insert into Pais(nombre) values('Saudi Arabia');
+insert into Pais(nombre) values('Senegal');
+insert into Pais(nombre) values('Serbia');
+insert into Pais(nombre) values('Seychelles');
+insert into Pais(nombre) values('Sierra Leone');
+insert into Pais(nombre) values('Singapore');
+insert into Pais(nombre) values('Slovakia');
+insert into Pais(nombre) values('Slovenia');
+insert into Pais(nombre) values('Solomon Islands');
+insert into Pais(nombre) values('Somalia');
+insert into Pais(nombre) values('South Africa');
+insert into Pais(nombre) values('South Sudan');
+insert into Pais(nombre) values('Spain');
+insert into Pais(nombre) values('Sri Lanka');
+insert into Pais(nombre) values('Sudan');
+insert into Pais(nombre) values('Suriname');
+insert into Pais(nombre) values('Swaziland');
+insert into Pais(nombre) values('Sweden');
+insert into Pais(nombre) values('Switzerland');
+insert into Pais(nombre) values('Syria');
+insert into Pais(nombre) values('Taiwan');
+insert into Pais(nombre) values('Tajikistan');
+insert into Pais(nombre) values('Tanzania');
+insert into Pais(nombre) values('Thailand');
+insert into Pais(nombre) values('Togo');
+insert into Pais(nombre) values('Tonga');
+insert into Pais(nombre) values('Trinidad & Tobago');
+insert into Pais(nombre) values('Tunisia');
+insert into Pais(nombre) values('Turkey');
+insert into Pais(nombre) values('Turkmenistan');
+insert into Pais(nombre) values('Tuvalu');
+insert into Pais(nombre) values('Uganda');
+insert into Pais(nombre) values('Ukraine');
+insert into Pais(nombre) values('United Arab Emirates');
+insert into Pais(nombre) values('United Kingdom');
+insert into Pais(nombre) values('United States');
+insert into Pais(nombre) values('Uruguay');
+insert into Pais(nombre) values('Uzbekistan');
+insert into Pais(nombre) values('Vanuatu');
+insert into Pais(nombre) values('Vatican City');
+insert into Pais(nombre) values('Venezuela');
+insert into Pais(nombre) values('Vietnam');
+insert into Pais(nombre) values('Yemen');
+insert into Pais(nombre) values('Zambia');
+insert into Pais(nombre) values('Zimbabwe');
+
+
+
+— CLiente
+insert into cliente(nombre,apellido_paterno,apellido_materno,id_pais) 
+		values('Alejandro','Saenz','Colque',1);
+insert into cliente(nombre,apellido_paterno,apellido_materno,id_pais) 
+		values('Kael','Silvez','Paramo',2);
+insert into cliente(nombre,apellido_paterno,apellido_materno,id_pais) 
+		values('Luis','Aviles','Ortiz',3);
+insert into cliente(nombre,apellido_paterno,apellido_materno,id_pais) 
+		values('Lu','Maida','Flores',4);
+insert into cliente(nombre,apellido_paterno,apellido_materno,id_pais) 
+		values('Ricolas','Miranda','Vasquez',5);
+insert into cliente(nombre,apellido_paterno,apellido_materno,id_pais) 
+		values('Lucio','Gonzales','Miranda',6);
+insert into cliente(nombre,apellido_paterno,apellido_materno,id_pais) 
+		values('Blanca','Salinas','Ortuno',7);
+insert into cliente(nombre,apellido_paterno,apellido_materno,id_pais) 
+		values('Ivy','Estrada','Fuentes',8);
+insert into cliente(nombre,apellido_paterno,apellido_materno,id_pais) 
+		values('Gissel','Saret','Polo',9);
+insert into cliente(nombre,apellido_paterno,apellido_materno,id_pais) 
+		values('Mirna','Platsi','Nina',10);
+
+-- tipo_personal
+insert into tipo_personal(nombre) values('seguridad');
+insert into tipo_personal(nombre) values('limpieza');
+insert into tipo_personal(nombre) values('cocina');
+insert into tipo_personal(nombre) values('recepcionista');
+insert into tipo_personal(nombre) values('botones');
+insert into tipo_personal(nombre) values('mantenimiento');
+insert into tipo_personal(nombre) values('transporte');
+
+-- tipo_habitaciones
+insert into tipo_habitacion(nombre) values('simple');
+insert into tipo_habitacion(nombre) values('doble');
+insert into tipo_habitacion(nombre) values('matrimonial');
+insert into tipo_habitacion(nombre) values('suite');
+insert into tipo_habitacion(nombre) values('familiar');
+
+-- tipo_especial 
+insert into tipo_especial(nombre) values('sauna');
+insert into tipo_especial(nombre) values('piscina');
+insert into tipo_especial(nombre) values('salon de baile');
+insert into tipo_especial(nombre) values('salon de te');
+insert into tipo_especial(nombre) values('area de juegos');
+
+--sucursal
+insert into sucursal(lugar,direccion) values('Cochabamba','Av.Libertador');
+insert into sucursal(lugar,direccion) values('Buenos Aires','Av.Campestre');
+insert into sucursal(lugar,direccion) values('Paris','Le pierre square');
+insert into sucursal(lugar,direccion) values('Texas','Route 84');
+insert into sucursal(lugar,direccion) values('Sidney','Av.Bourne');
+insert into sucursal(lugar,direccion) values('Madrid','Av.Principal');
+insert into sucursal(lugar,direccion) values('Tokyo','Maronouchi');
+
+--ambiente
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(1,'Habitacion 101','Habitacion simple con balcon',1);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(2,'Salon de juegos','Sala de juegos arcade',100);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(3,'Habitacion 201','HAbitacion doble',2);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(4,'Salon de te','Sala habilitada con mesas para te',40);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(5,'Sauna','Sauna seco y vapor',25);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(6,'Piscina','piscina atemperada',10);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(7,'Salon de baile','Salon con piso de madera para baile',150);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(1,'Habitacion 102','Habitacion matrimonial',2);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(1,'Sauna','Sauna seco y vapor',25);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(1,'Area de juegos','area de juegos arcade',25);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(1,'Habitacion 103','Suite',10);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(1,'Habitacion 104','Habitacion simple',1);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(1,'Habitacion 105','Habitacion doble',2);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(2,'Habitacion 205','Habitacion doble',2);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(2,'Habitacion 206','Suite',10);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(2,'Sauna','Sauna seco y vapor',14);
+insert into ambiente(id_sucursal,nombre,descripcion,capacidad)
+	values(3,'Salon de juegoes','Area de juegos arcade',25);
