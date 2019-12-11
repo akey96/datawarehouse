77c84899564e51c83cf4ee4f9c8d3deb0a305890
YaninaPaola101 <luriannemarianne@gmail.com>
2019-12-11 10:41:57 -0400

    correccion de fecha a la 3ra pregunta

--- 2doParcial/DB/queriesVistas.sql
+++ 2doParcial/DB/queriesVistas.sql
@@ -5,6 +5,7 @@
 -- 3ra pregunta 
 select s.lugar,sum(p.monto) from sucursal s, ambiente a, reserva r, pago p
 where  
+p.fecha >= '2017-01-01' and p.fecha < '2019-01-01' and
 s.id_sucursal = a.id_ambiente and
 a.id_ambiente = r.id_ambiente and
 r.id_reserva = p.id_reserva
